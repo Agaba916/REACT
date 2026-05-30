49ba6dcbde057be34ae02fd528ecfcf308b2d704
Agaba916 <agabahappy7@gmail.com>
2026-05-30 10:39:40 +0200

    Lists with React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,42 @@
-import {useState} from 'react';
-import './Signup.css';
+import './App.css';
 
-function SignupForm(){
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [age, setAge]= useState('')
-  const [password, setPassword] = useState('')
-
+function UserCard({id, name, age, city, isOnline}) {
   return (
-    <div className='form-container'>
-     <h2>Sign Up</h2>
-
-     <input
-     type='text'
-     placeholder='Name'
-     value={name}
-     onChange={(e)=>setName(e.target.value)}
-     />
-    <input
-    type='email'
-    placeholder='Email'
-    value={email}
-    onChange={(e)=>setEmail(e.target.value)}
-    />
-    <input
-    type= 'number'
-    placeholder='Age'
-    value={age}
-    onChange={(e)=>setAge(e.target.value)}
-    />
-    <input
-    type='password'
-    placeholder='Password'
-    value={password}
-    onChange={(e)=>setPassword(e.target.value)}
-    />
-    <button type='submit'>Sign Up</button>
-    </div>
-  )
+    <div className="user-card">
+      <div>
+        <h2>{name}</h2>
+        <p>Age: {age}</p>
+        <p>City: {city}</p>
+        </div>
+        <span className='status'> {isOnline ? '🟢' : '🔴'}</span>
+      </div>
+  );
 }
 
 function App(){
+  const users = [
+    {id: 1, name: 'Ahje', age: 16, city: 'New York', isOnline: true},
+    {id: 2, name: 'John', age: 25, city: 'Los Angeles', isOnline: false},
+    {id: 3, name: 'Emily', age: 30, city: 'Chicago', isOnline: true},
+    {id: 4, name: 'Michael', age: 22, city: 'Houston', isOnline: false},
+    {id: 5, name: 'Sarah', age: 28, city: 'Phoenix', isOnline: true}
+  ];
+
   return (
-    <div className='App'>\
-      <SignupForm />
+    <div className='users-container'>
+      <h1>Users List</h1>
+      {users.map((user)=>(
+        <UserCard
+        key={user.id}
+        name = {user.name}
+        age = {user.age}
+        city = {user.city}
+        isOnline = {user.isOnline}
+        />
+      ))}
     </div>
-  )
+
+  );
 }
 
 export default App;
