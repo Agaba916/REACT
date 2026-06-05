4f1caee1be0e5c98e05959baa07fd8058eb54295
Agaba916 <agabahappy7@gmail.com>
2026-06-05 10:03:30 +0200

    Add filtering with search

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,60 @@
+import { useState } from 'react';
 import './App.css';
 
-function UserCard({id, name, age, city, isOnline}) {
+function UserCard({name, age , city, isOnline}){
   return (
-    <div className="user-card">
+    <div className='user-card'>
       <div>
         <h2>{name}</h2>
         <p>Age: {age}</p>
         <p>City: {city}</p>
-        </div>
-        <span className='status'> {isOnline ? '🟢' : '🔴'}</span>
-      </div>
+         </div>
+        <span className= 'status'>
+          {isOnline ? '🟢' : '🔴'}
+        </span>
+    </div>
   );
 }
 
 function App(){
+  const [search, setSearch]=useState('');
+  const [isOnlineOnly, setIsOnlineOnly] = useState(false);
   const users = [
     {id: 1, name: 'Ahje', age: 16, city: 'New York', isOnline: true},
-    {id: 2, name: 'John', age: 25, city: 'Los Angeles', isOnline: false},
-    {id: 3, name: 'Emily', age: 30, city: 'Chicago', isOnline: true},
-    {id: 4, name: 'Michael', age: 22, city: 'Houston', isOnline: false},
-    {id: 5, name: 'Sarah', age: 28, city: 'Phoenix', isOnline: true}
+    {id: 2, name: 'Ahmet', age: 20, city: 'Los Angeles', isOnline: false},
+    {id: 3, name: 'Ayşe', age: 25, city: 'Chicago', isOnline: true},
+    {id: 4, name: 'Mehmet', age: 30, city: 'Houston', isOnline: false},
   ];
 
-  return (
-    <div className='users-container'>
-      <h1>Users List</h1>
-      {users.map((user)=>(
-        <UserCard
-        key={user.id}
-        name = {user.name}
-        age = {user.age}
-        city = {user.city}
-        isOnline = {user.isOnline}
-        />
-      ))}
-    </div>
+const filteredUsers = users.filter((user)=> 
+user.name.toLowerCase().includes(search.toLowerCase())
+)
+.filter((user)=> (isOnlineOnly ? user.isOnline:true));
+return (
+  <div className='app'>
+  <h1> Users List</h1>
+  <input type='text' 
+  placeholder = 'Search by name...'
+  value={search}
+  onChange={(e)=>setSearch(e.target.value)}
+  />
 
-  );
+  <button onClick={()=> setIsOnlineOnly(!isOnlineOnly)}>
+    {isOnlineOnly ? 'Show All' : 'Show Online Only'}
+  </button>
+  <p> Showing {filteredUsers.length} users</p>
+  {filteredUsers.length === 0 && <p> No users found!</p>}
+  {filteredUsers.map((user)=>(
+    <UserCard
+    key={user.id}
+    name = {user.name}
+    age = {user.age}
+    city = {user.city}
+    isOnline= {user.isOnline}
+    />
+  ))}
+  </div>
+);
 }
 
 export default App;
